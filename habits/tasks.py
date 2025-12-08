bd4c3673efbbb4b42e18c3e80db972893b899752
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-12-09 02:17:56 +0700

    modified shared task send_habit_reminder

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from datetime import date
 from celery import shared_task
 from django.utils import timezone
@@ -9,13 +8,14 @@ from habits.services import send_telegram_message
 @shared_task
 def send_habit_reminder():
     """Отправляет пользователю напоминание о ближайшей привычке через телеграм-бота"""
+
     now = timezone.localtime()
     now_time = now.time().replace(second=0, microsecond=0)
 
-    habits = Habit.objects.filter(habit_time=now_time)
+    habits = Habit.objects.filter(habit_time=now_time, user__tg_chat_id__isnull=False).select_related("user")
 
     for habit in habits:
-        days_passed = (date.today() - habit.created_at.date()).days
-        if days_passed % habit.periodicity == 0:
+        date_passed = (date.today() - habit.created_at.date()).days
+        if date_passed % habit.periodicity == 0:
             message = f"Время выполнить привычку: {habit.action}. Это займет всего 2 минуты — ты справишься!"
-            send_telegram_message(habit.user.tg_chat_id, message)
+            send_telegram_message.delay(habit.user.tg_chat_id, message)
