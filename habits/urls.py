217d921241607c7de9ee7904d54b8ca63bcb54a7
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-12-08 16:56:18 +0700

    added viewset, urlpatterns, pagonators, validators, serializers for habits app

--- habits/urls.py
+++ habits/urls.py
@@ -3,14 +3,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from habits.apps import HabitsConfig
-
+from habits.views import HabitsViewSet
 
 app_name = HabitsConfig.name
 
 router = DefaultRouter()
-# router.register(r"habits", HabitsViewSet, basename="habits")
+router.register(r"habits", HabitsViewSet, basename="habits")
 
 urlpatterns = [
-    # path("", include(router.urls)),
+    path("", include(router.urls)),
 ]
-
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitsPaginator
+from users.permissions import IsOwner
+
+
+class HabitsViewSet(viewsets.ModelViewSet):
+    """Вьюсет привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitsSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["periodicity", "habit_time"]
+    pagination_class = HabitsPaginator
+
+    def get_queryset(self):
+        """
+        Возвращает только привычки текущего пользователя
+        Суперпользователь видит все привычки
+        """
+
+        if self.request.user.is_superuser:
+            return Habit.objects.all()
+        return Habit.objects.filter(owner=self.request.user)
+
+
+    @action(detail=False, methods=["get"], url_path="public", permission_classes=[IsAuthenticated])
+    def public_habits(self, request):
+        """Возвращает список публичных привычек всех пользователей для авторизованных пользователей"""
+
+        queryset = Habit.objects.filter(is_public=True).order_by("action", "habit_time", "place")
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = HabitsSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = HabitsSerializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+
+    def perform_create(self, serializer):
+        """При создании привычки устанавливает пользователя как владельца"""
+
+        serializer.save(owner=self.request.user)
+
+
+    def get_permissions(self):
+        """Определяет права на действия с привычками для разных уровней пользователей:
+        - авторизованный суперпользователь - все права;
+        - авторизованный пользователь - просмотр списка публичных привычек, создание своих привычек;
+        - авторизованный владелец - просмотр, изменение, удаление своих привычек;
+        """
+
+        if self.request.user.is_superuser:
+            return []
+        if self.action in ["retrieve", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwner()]
+        return [IsAuthenticated()]
